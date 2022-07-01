fcacbe18cbd93dbdbe140be4347697c131eb86e5
Yevhen Bashchak <evgeny221@gmail.com>
2022-07-01 12:11:05 +0200

    add function

--- src/index.js
+++ src/index.js
@@ -16,40 +16,24 @@ function inputCountry() {
     
 fetchCountries(searchCountry).then(data => {
     return  data; })
-.then(createListOfCountries)
+.then(renderResult)
 .catch(error =>{ console.log(error);
 clearCardCountry();
 clearList();
-Notify.failure("Oops, there is no country with that name");})
+massageError();})
 };
-
-function createListOfCountries(data) {
+function renderResult(data) {
     if (data.length >= 10){
+        massageInfo();
         clearCardCountry();
         clearList();
-        Notify.info("Too many matches found. Please enter a more specific name.");
     } if (data.length > 1 && data.length < 10){
         clearCardCountry();
-        listCountry.innerHTML = data.map(({flags, name}) =>   
-        ` <li class="country-list__item">
-        <img src="${flags.svg}" alt="прапор" height="20" width="20"/>
-        <p class="text">${name.official}</p>
-        </li>`).join("");
-
+        renderList(data);
     } if (data.length === 1){
-         clearList() 
-        cardCountry.innerHTML = data.map(({name,capital,population,languages,flags})=> 
-        `<h1 class="country-list__item">
-        <img src="${flags.svg}" alt="прапор" width="40"/>
-        ${name.official}</h1>
-        <p class="text">Capital: ${capital}</p>
-        <p class="text">Population: ${population}</p>
-        <p class="text">Languages: ${Object.values(languages)}</p>
-        `).join("");   
+         clearList();
+         renderCard(data);
 }}
-//  
-
-
 
 function clearList() {
     listCountry.innerHTML = "";
@@ -58,3 +42,27 @@ function clearList() {
   cardCountry.innerHTML = "";
  }
 
+function massageInfo() {
+    Notify.info("Too many matches found. Please enter a more specific name.");
+};
+function massageError() {
+    Notify.failure("Oops, there is no country with that name");
+}
+function renderList(data) {
+    listCountry.innerHTML = data.map(({flags, name}) =>   
+        ` <li class="country-list__item">
+        <img src="${flags.svg}" alt="прапор" height="20" width="20"/>
+        <p class="text">${name.official}</p>
+        </li>`).join("");
+};
+
+function renderCard(data) {
+   cardCountry.innerHTML = data.map(({name,capital,population,languages,flags})=> 
+    `<h1 class="country-list__item">
+    <img src="${flags.svg}" alt="прапор" width="40"/>
+    ${name.official}</h1>
+    <p class="text">Capital: ${capital}</p>
+    <p class="text">Population: ${population}</p>
+    <p class="text">Languages: ${Object.values(languages)}</p>
+    `).join("");   
+}
